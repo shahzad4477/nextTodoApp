db3d373c145f7ebdd002cd480e8210bae46bd731
Muhammad Shahzad <m.shahzad4477@gmail.com>
2025-02-18 20:31:11 +0500

    one div removed in master

--- src/component/dashboard/Dashboard.tsx
+++ src/component/dashboard/Dashboard.tsx
@@ -2,9 +2,7 @@ const Dashboard = () => {
     return ( <>
     <div>Hello fixes</div>
     <div>Hello </div>
-    <div>Hello 2</div>
-    <div>Hello</div>
-    
+    <div>Hello 2</div> 
     <div>Hello 3</div>
     </> );
 }
